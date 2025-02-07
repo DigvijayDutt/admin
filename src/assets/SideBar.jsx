c37f615921c3b97b9be5281688fb22fcf4adb506
DigvijayDutt <duttdigvijay@gmail.com>
2025-02-07 16:36:32 +0530

    minor

--- src/assets/SideBar.jsx
+++ src/assets/SideBar.jsx
@@ -4,12 +4,12 @@ import "./Sidebar.css";
 function SideBar() {
     return(
         <div className="sidebar">
-            <h2>Admin Menu</h2>
+            <h2><Link to="/dash">Admin Menu</Link></h2>
             <ul>
                 <li><Link to="/coursemanage">Courses</Link></li>
                 <li><Link to="/learning">Domains</Link></li>
                 <li><Link to="/usermanage">Users</Link></li>
-                <li><Link to="/reports">Reports</Link></li>
+                <li><Link to="">Reports</Link></li>
             </ul>
         </div>
     );
