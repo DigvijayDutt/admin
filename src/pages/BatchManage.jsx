ab681b8283d1bf257aa870ce929fd4721315536e
DigvijayDutt <duttdigvijay@gmail.com>
2025-02-27 17:12:22 +0530

    batchmanage front complete

--- src/pages/BatchManage.jsx
+++ src/pages/BatchManage.jsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from "react";
 import NavBar from "../assets/Navbar";
 import SideBar from "../assets/SideBar";
 import '../styles/BatchManage.css';
+import axios from "axios";
 function BatchManage(){
+    const [batches, setBatches] = useState([]);
+    useEffect(()=>{
+        const fetchData= async ()=>{
+            try{
+                const [batches] = await Promise.all([
+                    axios.get("http://localhost:5000/batches")
+                ]);
+            } catch(e){
+                console.log(e);
+            }
+        }
+    })
     return(
         <>
             <div className="BMcontainer">
@@ -29,6 +43,34 @@ function BatchManage(){
                        <label htmlFor="inactive">In-Active</label>
                        <button type="submit">Add batch</button>
                     </form>
+                </div>
+                <div className="BMtable">
+                    <table>
+                        <thead>
+                            <th>Course Name</th>
+                            <th>Instructor Name</th>
+                            <th>Start Date</th>
+                            <th>End Date</th>
+                            <th>Duration</th>
+                            <th>Seats</th>
+                            <th>Status</th>
+                        </thead>
+                        <tbody>
+                            {batches.length===0 ? (<p>No courses available.</p>) : (
+                                batches.map((batch)=>(
+                                    <tr key={batch.batchId}>
+                                        <td>{batch.Cname}</td>
+                                        <td>{batch.Iname}</td>
+                                        <td>{batch.Sdate}</td>
+                                        <td>{batch.Edate}</td>
+                                        <td>{batch.Dur}</td>
+                                        <td>{batch.seats}</td>
+                                        <td>{batch.status}</td>
+                                    </tr>
+                                ))
+                            )}
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </>
