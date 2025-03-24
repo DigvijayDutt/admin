7b90ac89eab317a6825884d6e48f4d17016d81d2
DigvijayDutt <duttdigvijay@gmail.com>
2025-03-24 18:06:07 +0530

    popup css fixed

--- src/assets/ErrorPopup.jsx
+++ src/assets/ErrorPopup.jsx
@@ -1,21 +1,22 @@
 import { useState } from "react";
 import { Alert, AlertTitle } from "@mui/material";
 import { X } from "lucide-react";
-
+import './ErrorPopup.css';
+import 'react-bootstrap';
 const ErrorPopup = () => {
   const [visible, setVisible] = useState(true);
 
   if (!visible) return null;
 
   return (
-    <div className="fixed top-5 right-5 bg-red-500 text-white p-4 rounded-lg shadow-lg flex items-center gap-3">
-      <Alert severity="error" className="bg-red-500 text-white">
-        <AlertTitle>Error</AlertTitle>
+    <div className="">
+      <Alert severity="error" className="alert">
+        <AlertTitle className="Etitle">Error</AlertTitle>
         An Error Occurred
+        <button onClick={() => setVisible(false)} className="x">
+        <X size={15} />
+        </button>
       </Alert>
-      <button onClick={() => setVisible(false)} className="text-white">
-        <X size={10} />
-      </button>
     </div>
   );
 };
